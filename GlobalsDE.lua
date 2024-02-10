8c8f4ae0b9b0700ab99648d7b5f50c6df51642a8
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-10 15:29:14 +0100

    Create GlobalsDE.lua

---
+++ GlobalsDE.lua
@@ -0,0 +1,239 @@
+------------------------------------------------------------------------------------------
+-- GlobalDE file
+-- Written by Homeopatix
+-- 17 avril 2021
+------------------------------------------------------------------------------------------
+_G.T = {};
+	------------------------------------------------------------------------------------------
+	-- Deutsche --
+	------------------------------------------------------------------------------------------
+	T[ "PluginName" ] = "Tokens";
+	T[ "PluginText" ] = "Von Homeopatix";
+	T[ "Loaded" ] = "Geladen";
+	T[ "Unactivated" ] = "Deaktiviert";
+	T[ "PluginEscEnable" ] = "Escape-Taste aktiviert";
+	T[ "PluginEscDesable" ] = "Escape-Taste deaktiviert";
+	T[ "PluginAltEnable" ] = "Alt-Taste aktiviert";
+	T[ "PluginAltDesable" ] = "Alt-Taste deaktiviert";
+	T[ "PluginToggleOn" ] = "Fenster aktivieren";
+	T[ "PluginToggleOff" ] = "Fenster deaktivieren";
+	T[ "PluginEscIconOn" ] = "Zeige das Symbol an";
+	T[ "PluginEscIconOff" ] = "Verstecke das Symbol";
+	T[ "PluginWindowShow" ] = "Schaufenster";
+	T[ "PluginWindowHide" ] = "Verstecke das Fenster";
+	T[ "ComptePartage" ] = "Gemeinsames Konto";
+	T[ "PluginClear" ] = "Fenster neu initialisieren";
+	T[ "PluginOptionsWindowShow" ] = "Optionsfenster anzeigen";
+	------------------------------------------------------------------------------------------
+	-- debug --
+	------------------------------------------------------------------------------------------
+	T[ "PluginDebugOff" ] = "Modus Debug aus";
+	T[ "PluginDebugOn" ] = "Modus-Debugging an";
+	T[ "PluginDebugTexte1" ] = "Wenn der Debug-Modus aktiviert ist; speichern die Plugins die Namen aller Ihrer Token in einer Datei\n";
+	T[ "PluginDebugTexte2" ] = "das befindet sich in : \n";
+	T[ "PluginDebugTexte3" ] = "C:\Users\<DeinBenutzername>\Documents\The Lord of the Rings Online\PluginData\<IhrKonto>\<ServerName>\<CharacterName>\n";
+	T[ "PluginDebugTexte4" ] = "Die Datei hei\195\159t ";
+	T[ "PluginDebugTexte5" ] = "TokensNamesDE.plugindata";
+	T[ "PluginDebugTexte6" ] = "Wenn Sie die Sprache \195\164ndern; wird die Datei in der Sprache gespeichert; in der Sie eine Verbindung herstellen\n";
+	T[ "PluginDebugTexte7" ] = "Sie k\195\180nnen es mir senden; damit ich die fehlenden Token hinzuf\195\188gen kann...\n";
+	T[ "PluginSave" ] = "Debug-Datei gespeichert";
+	------------------------------------------------------------------------------------------
+	-- help --
+	------------------------------------------------------------------------------------------
+	T[ "PluginHelp1" ] = "\n\nListe der Befehle:\n";
+	T[ "PluginHelp2" ] = "/Tok help - Hilfefenster anzeigen.\n";
+	T[ "PluginHelp3" ] = "/Tok show - Fenster anzeigen.\n";
+	T[ "PluginHelp4" ] = "/Tok hide - Fenster verbergen.\n";
+	T[ "PluginHelp5" ] = "/Tok esc - Escape-Taste aktivieren oder deaktivieren.\n";
+	T[ "PluginHelp6" ] = "/Tok alt - Alt-Taste aktivieren oder deaktivieren; um das Symbol zu verschieben.\n";
+	T[ "PluginHelp7" ] = "/Tok toggle - Fenster aktivieren- und deaktivieren.\n";
+	T[ "PluginHelp8" ] = "/Tok options - Optionsfenster anzeigen.\n";
+	T[ "PluginHelp9" ] = "/Tok clear - Fenster neu initialisieren.\n\n";
+	T[ "PluginCloseButton" ] = "Fenster schlie\195\159en";
+	------------------------------------------------------------------------------------------
+	-- options window --
+	------------------------------------------------------------------------------------------
+	T[ "PluginOptionsText" ] = "Optionsfenster";
+	T[ "PluginOptionTokens1" ] = "Token zum Anzeigen";
+	T[ "PluginOptionTokens0" ] = "W\195\164hlen Sie aus, welche Token angezeigt werden sollen";
+	T[ "PluginOptionValidate" ] = "\195\132nderungen validieren";
+	T[ "PluginOptionDelete" ] = "Fenster neu initialisieren";
+	------------------------------------------------------------------------------------------
+	-- Wallet Currency Controls --
+	------------------------------------------------------------------------------------------
+	T[ "TokenContent1" ] = "Pi\195\168ce";
+	T[ "TokenContent2" ] = "Paysage";
+	T[ "TokenContent3" ] = "Festival et Evenements";
+	T[ "TokenContent4" ] = "Progression d'objet";
+	T[ "TokenContent5" ] = "Autres";
+	T[ "TokenContent6" ] = "Artisanats";
+	T[ "TokenContent7" ] = "Taxidermiste";
+	T[ "TokenContent8" ] = "Instances et Escarmouches";
+	T[ "TokenContent9" ] = "Monnaie";
+	------------------------------------------------------------------------------------------
+	-- Piece -- 1
+	------------------------------------------------------------------------------------------
+	T[ "TokenPiece1" ] = "Mithril-M\195\188nze";
+	------------------------------------------------------------------------------------------
+	-- Paysage -- 55
+	------------------------------------------------------------------------------------------
+	T[ "TokenPaysage1" ] = "Silberzweig aus Lothl\195\179rien";
+	T[ "TokenPaysage2" ] = "Goldenes Malledhrim-Sternabzeichen";
+	T[ "TokenPaysage3" ] = "Phiole mit bernsteinfarbenem Extrakt";
+	T[ "TokenPaysage4" ] = "Phiole mit karmesinrotem Extrakt";
+	T[ "TokenPaysage5" ] = "Phiole mit umbrafarbenem Extrakt";
+	T[ "TokenPaysage6" ] = "Phiole mit violettem Extrakt";
+	T[ "TokenPaysage7" ] = "Phiole mit gr\195\188nem Extrakt";
+	T[ "TokenPaysage8" ] = "G\195\186mark";
+	T[ "TokenPaysage9" ] = "N\195\182rdliche G\195\186mark";
+	T[ "TokenPaysage10" ] = "Geode (Achat)";
+	T[ "TokenPaysage11" ] = "Geode (Amethyst)";
+	T[ "TokenPaysage12" ] = "Geode (Gips)";
+	T[ "TokenPaysage13" ] = "Geode (Jaspis)";
+	T[ "TokenPaysage14" ] = "Geode (Quarz)";
+	T[ "TokenPaysage15" ] = "Silbernes Siegel der Thandrim";
+	T[ "TokenPaysage16" ] = "Silberne M\195\188nze vom Anduin";
+	T[ "TokenPaysage17" ] = "Silberm\195\188nze aus der Riddermark";
+	T[ "TokenPaysage18" ] = "M\195\188nze von Hytbold";
+	T[ "TokenPaysage19" ] = "M\195\188\nze von See und Fl\195\188ssen";
+	T[ "TokenPaysage20" ] = "Silberm\195\188\nze der Wildnis";
+	T[ "TokenPaysage21" ] = "Goldm\195\188nze der Wildnis";
+	T[ "TokenPaysage22" ] = "Gabil'akk\195\162-Kriegsmarke";
+	T[ "TokenPaysage23" ] = "Marke der Langb\195\164rte";
+	T[ "TokenPaysage24" ] = "Marke der Wildnis";
+	T[ "TokenPaysage25" ] = "Minas Tirith - M\195\188nze der Bauarbeiter";
+	T[ "TokenPaysage26" ] = "Amroth-Silberst\195\188ck";
+	T[ "TokenPaysage27" ] = "Silberm\195\188nze von Gundabad";
+	T[ "TokenPaysage28" ] = "Heer des Westens - Silberst\195\188ck";
+	T[ "TokenPaysage29" ] = "Silberst\195\188\ck aus Minas Tirith";
+	T[ "TokenPaysage30" ] = "Zentralgondorisches Silberst\195\188ck";
+	T[ "TokenPaysage31" ] = "Ostgondorisches Silberst\195\188ck";
+	T[ "TokenPaysage32" ] = "Kupferm\195\188nze von Gundabad";
+	T[ "TokenPaysage33" ] = "Eisenm\195\188nze aus West-Emnet";
+	T[ "TokenPaysage34" ] = "Bronzene Malledhrim-Feder";
+	T[ "TokenPaysage35" ] = "Siegel von Imlad Ithil";
+	T[ "TokenPaysage36" ] = "Marke der Kharum-ubn\195\162r";
+	T[ "TokenPaysage37" ] = "Phiole mit goldenem Extrakt";
+	T[ "TokenPaysage38" ] = "Kampfgruppen-M\195\188nze der Wildermark";
+	T[ "TokenPaysage39" ] = "Goldene M\195\188nze der Wildermark";
+	T[ "TokenPaysage40" ] = "Wildermark-M\195\188nze";
+	T[ "TokenPaysage41" ] = "T\195\164ler - Elben-M\195\188nze";
+	T[ "TokenPaysage42" ] = "T\195\164ler - Waldmenschen-Marke";
+	T[ "TokenPaysage43" ] = "T\195\164ler - Beorninger-M\195\188nze";
+	T[ "TokenPaysage44" ] = "Minas Tirith - M\195\188nze der Schmiede";
+	T[ "TokenPaysage45" ] = "Zeichen des Schenkenden";
+	T[ "TokenPaysage46" ] = "Fangornblatt";
+	T[ "TokenPaysage47" ] = "Goldm\195\188nze aus der Riddermark";
+	T[ "TokenPaysage48" ] = "Goldene M\195\188nze vom Anduin";
+	T[ "TokenPaysage49" ] = "Lang verloren geglaubte M\195\188nze";
+	T[ "TokenPaysage50" ] = "M\195\188nze von Gr\195\161rik";
+	T[ "TokenPaysage51" ] = "Zakaf-beshek";
+	T[ "TokenPaysage52" ] = "Marke weiterer Abenteuer";
+	T[ "TokenPaysage53" ] = "Eisenm\195\188nze von Cardolan";
+	T[ "TokenPaysage54" ] = "Zeichen des Wiederaufbaus";
+	T[ "TokenPaysage55" ] = "Heldenmarke";
+	------------------------------------------------------------------------------------------
+	-- Festival et evenements -- 9
+	------------------------------------------------------------------------------------------
+	T[ "TokenFestival1" ] = "Schildm\195\188nze";
+	T[ "TokenFestival2" ] = "Jubil\195\164umsm\195\188nze";
+	T[ "TokenFestival3" ] = "Herbstfest-Medaille";
+	T[ "TokenFestival4" ] = "Julfest-M\195\188nze";
+	T[ "TokenFestival5" ] = "Teil einer Essenzr\195\188ckgewinnungs-Schriftrolle";
+	T[ "TokenFestival6" ] = "Fr\195\188hlingsblatt";
+	T[ "TokenFestival7" ] = "Siegel der Hingabe";
+	T[ "TokenFestival8" ] = "Mittsommer-Marken";
+	T[ "TokenFestival9" ] = "Bauernfest-M\195\188nzen";
+	T[ "TokenFestival10" ] = "Festm\195\188nze";
+	T[ "TokenFestival11" ] = "M\195\188nze der vergrabenen Sch\195\164tze";
+	T[ "TokenFestival12" ] = "Gro\195\159er Fisch";
+	T[ "TokenFestival13" ] = "M\195\182glicher Fisch";
+	T[ "TokenFestival14" ] = "Schlichter Fisch";
+	T[ "TokenFestival15" ] = "Mickriger Fisch";
+	T[ "TokenFestival16" ] = "Einfacher Fisch";
+	T[ "TokenFestival17" ] = "Ungenie\195\159barer Fisch";
+	T[ "TokenFestival18" ] = "Goldene Festm\195\188nze";
+	T[ "TokenFestival19" ] = "Bierflasche";
+	------------------------------------------------------------------------------------------
+	-- Progression d'objet -- 4
+	------------------------------------------------------------------------------------------
+	T[ "TokenProgression1" ] = "Scherbe";
+	T[ "TokenProgression2" ] = "Rostige Zwergenwerkzeuge";
+	T[ "TokenProgression3" ] = "Khuzdul-Tafeln";
+	T[ "TokenProgression4" ] = "Uralte Schrift";
+	------------------------------------------------------------------------------------------
+	-- Autres -- 2
+	------------------------------------------------------------------------------------------
+	T[ "TokenAutres1" ] = "Anerkennung";
+	T[ "TokenAutres2" ] = "Eisenm\195\188nze des Gru\195\159es";
+	T[ "TokenAutres3" ] = "Leuchtendes Zeichen von Nimrodel";
+	T[ "TokenAutres4" ] = "Schwarzer Schl\195\188ssel";
+	T[ "TokenAutres5" ] = "\195\132ltestental-Vorratspaket";
+	T[ "TokenAutres6" ] = "Marke der Willenskraft";
+	------------------------------------------------------------------------------------------
+	-- Artisanats -- 2
+	------------------------------------------------------------------------------------------
+	T[ "TokenArtisanats1" ] = "M\195\188nze f\195\188r Rohstoffe der Eisernen Garnison";
+	T[ "TokenArtisanats2" ] = "Universelles Zutatenb\195\188ndel";
+	T[ "TokenArtisanats3" ] = "Geschm\195\188ckter, schwarzer Schl\195\188ssel";
+	T[ "TokenArtisanats4" ] = "M\195\188nze der M\195\188he";
+	------------------------------------------------------------------------------------------
+	-- Taxidermiste -- 1
+	------------------------------------------------------------------------------------------
+	T[ "TokenTaxidermiste1" ] = "Ausgezeichneter Stint";
+	T[ "TokenTaxidermiste2" ] = "Unbesch\195\164digter Tundrab\195\164r-Kadaver";
+	T[ "TokenTaxidermiste3" ] = "Unbesch\195\164digter Winterwurm-Kadaver";
+	T[ "TokenTaxidermiste4" ] = "6-pf\195\188ndiger Lachs";
+	T[ "TokenTaxidermiste5" ] = "10-pf\195\188ndiger Lachs";
+	T[ "TokenTaxidermiste6" ] = "50-pf\195\188ndiger Lachs";
+	T[ "TokenTaxidermiste7" ] = "Hervorragender Hecht";
+	T[ "TokenTaxidermiste8" ] = "Fantastische Flunder";
+	T[ "TokenTaxidermiste9" ] = "Gro\195\159e Goldbarbe";
+	T[ "TokenTaxidermiste10" ] = "Gesprungener Taschenspiegel";
+	T[ "TokenTaxidermiste11" ] = "Norn\195\186ans Kopf";
+	------------------------------------------------------------------------------------------
+	-- Instances et Escarmouches -- 37
+	------------------------------------------------------------------------------------------
+	T[ "TokenInstances1" ] = "Zeichen";
+	T[ "TokenInstances2" ] = "Medaillon";
+	T[ "TokenInstances3" ] = "Siegel";
+	T[ "TokenInstances4" ] = "Stern des Verdienst";
+	T[ "TokenInstances5" ] = "Gro\195\159er Elbenstein der Entschlossenheit";
+	T[ "TokenInstances6" ] = "Gro\195\159er Elbenstein der St\195\164rke";
+	T[ "TokenInstances7" ] = "Gro\195\159er Elbenstein des Geistes";
+	T[ "TokenInstances8" ] = "Abgetragener Helm der Felder des Pelennor";
+	T[ "TokenInstances9" ] = "Meisterwerk-Helm der Felder des Pelennor";
+	T[ "TokenInstances10" ] = "Makelloser Ring der Felder des Pelennor";
+	T[ "TokenInstances11" ] = "Gl\195\188hend roter Rubin";
+	T[ "TokenInstances12" ] = "Flimmernder Amethyst";
+	T[ "TokenInstances13" ] = "Funkelnder Diamant";
+	T[ "TokenInstances14" ] = "Strahlender Smaragd";
+	T[ "TokenInstances15" ] = "Feuriger Quarz";
+	T[ "TokenInstances16" ] = "Nahezu perfekter Saphir";
+	T[ "TokenInstances17" ] = "Neuentdeckte M\195\188nze";
+	T[ "TokenInstances18" ] = "Quittung f\195\188r einen Waffentausch";
+	T[ "TokenInstances19" ] = "Morgul-Wappen";
+	T[ "TokenInstances20" ] = "Abscheuliche Silberm\195\188nze";
+	T[ "TokenInstances21" ] = "Abscheuliche Bronzem\195\188nze";
+	T[ "TokenInstances22" ] = "Orthanc-Siegel-Fragment";
+	T[ "TokenInstances23" ] = "M\195\188nze der b\195\182sen Omen";
+	T[ "TokenInstances24" ] = "Ungeschl\195\188pftes Spinnenei";
+	T[ "TokenInstances25" ] = "Medaillon von Lothl\195\179rien";
+	T[ "TokenInstances26" ] = "Zeichen des Siegs";
+	T[ "TokenInstances27" ] = "Zeichen des Triumphs";
+	T[ "TokenInstances28" ] = "Ud\195\186nions Horn";
+	T[ "TokenInstances29" ] = "Fetzen von Wisd\195\161ns Umhang";
+	T[ "TokenInstances30" ] = "Eglain-Zeichen";
+	T[ "TokenInstances31" ] = "Dunkles Zeichen der Entschlossenheit";
+	T[ "TokenInstances32" ] = "Marke der Zh\195\169lruka";
+	T[ "TokenInstances33" ] = "Medaillon von Moria";
+	T[ "TokenInstances34" ] = "Gundabad-Bergmarke";
+	T[ "TokenInstances35" ] = "Uralte Ithil-M\195\188nze";
+	T[ "TokenInstances36" ] = "Helchgams Schnabel";
+	T[ "TokenInstances37" ] = "Thr\195\162ngs Hortm\195\188nze";
+	------------------------------------------------------------------------------------------
+	-- Monnaie -- 3
+	------------------------------------------------------------------------------------------
+	T[ "TokenMonnaie1" ] = "Asche der Verzauberung";
+	T[ "TokenMonnaie2" ] = "Aschepartikel der Verzauberung";
+	T[ "TokenMonnaie3" ] = "Prachtst\195\188ck des Glanzes";
